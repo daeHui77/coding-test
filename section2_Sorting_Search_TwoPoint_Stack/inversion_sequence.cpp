2b5038adbdd6b76038f854995c177ccbb907d4d3
bin_my <seminassasla@naver.com>
2023-11-08 14:16:33 +0900

    02-38. Inversion Sequence(study)

---
+++ section2_Sorting_Search_TwoPoint_Stack/inversion_sequence.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	
+	int n, i, j, pos;
+	scanf("%d", &n);
+	vector<int> is(n + 1), os(n + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &is[i]);
+	}
+	
+	for (i = n; i >= 1; i--)
+	{
+		pos = i;
+		for (j = 1; j <= is[i]; j++)
+		{
+			os[pos] = os[pos + 1];
+			pos++;
+		}
+		os[pos] = i;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d", os[i]);
+	}
+
+}
