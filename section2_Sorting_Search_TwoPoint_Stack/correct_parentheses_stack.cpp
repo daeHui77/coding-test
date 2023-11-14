6eb48105e47b118931c7c1735c7f51fa77d7af72
bin_my <seminassasla@naver.com>
2023-11-14 12:16:01 +0900

    02-54. 올바른 괄호(stack)(solved)

---
+++ section2_Sorting_Search_TwoPoint_Stack/correct_parentheses_stack.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	char a[500];
+	stack<char> arr;
+	bool f = true;
+	scanf("%s", &a);
+
+	for (int i = 0; a[i]!= '\0'; i++)
+	{
+		
+		
+		if (a[i] == '(')
+		{
+			arr.push(a[i]);
+		}
+		else if (a[i] == ')')
+		{
+			if (arr.empty())
+			{
+				printf("%s", "NO");
+				return 0;
+			}else arr.pop();
+			
+		}
+	}
+	if (!arr.empty() )
+	{
+		printf("%s", "NO");
+	}
+	else if (arr.empty())
+	{
+		printf("%s", "YES");
+	}
+	
+	return 0;
+}
